ea672390dc37dc4ebb7b7bcdaec5ed08f05a3729
tchang <tlchang9@gmail.com>
2019-02-09 04:26:16 -0500

    Wrote #my_select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    selected = []
+    
+    while i < collection.length
+        testing = yield collection[i]
+        if testing == true
+            selected.push(collection[i]) 
+        end
+        i += 1
+    end
+    return selected
 end
