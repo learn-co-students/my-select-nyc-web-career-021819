b11df4dc31b20fc0c32d188dd3bc4f2dea795e5b
Nathaniel Cain <nathaniel2897@gmail.com>
2019-02-18 03:18:12 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if array.count == 0
+    return nil
+  else
+    selection = []
+    i = 0
+    while i < array.length
+      if array[i] % 2 == 0
+        yield array[i]
+        selection << array[i]
+        i = i + 1
+      else
+        puts array[i]
+        i = i + 1
+      end
+    end
+    return selection
+  end
 end
